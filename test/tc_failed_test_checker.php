a078eb74ed06066041b6d57c83826897e6d7e897
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-04-11 18:24:08 +0200

    Fix for PHP5.6

--- test/tc_failed_test_checker.php
+++ test/tc_failed_test_checker.php
@@ -5,11 +5,13 @@ class TcFailedTestChecker extends TcBase {
 		$run_unit_tests = $_SERVER["argv"][0];
 
 		$failed_test = escapeshellarg("!tc_failed_test.php");
-		exec("$run_unit_tests $failed_test",$output,$result_code);
+		$cmd = "$run_unit_tests $failed_test";
+		exec($cmd,$output,$result_code);
 		$this->assertEquals(1,$result_code);
 
 		$successful_test = escapeshellarg("tc_successful_test.php");
-		exec("$run_unit_tests $successful_test",$output,$result_code);
+		$cmd = "$run_unit_tests $successful_test";
+		exec($cmd,$output,$result_code);
 		$this->assertEquals(0,$result_code);
 	}
 }
